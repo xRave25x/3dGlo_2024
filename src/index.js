f6d7c15d2c8b283c775ccf1063d950eb8ff67347
Andrei Demichev <andyuson24@gmail.com>
2024-01-21 20:58:46 +0200

    выполнил задание с регуоярными выражениями для форм

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import menu from './modules/menu';
 import timer from './modules/timer';
 import modal from './modules/modal';
+import calc from './modules/calc';
 
 timer('23 january 2024');
 menu();
 modal();
+calc();
