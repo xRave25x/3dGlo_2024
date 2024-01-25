7e74d34218a118501694239fbd79fa2ec84360ec
Andrei Demichev <andyuson24@gmail.com>
2024-01-25 22:59:21 +0200

    реализовал слайдер

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import timer from './modules/timer';
 import modal from './modules/modal';
 import calc from './modules/calc';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer('23 january 2024');
 menu();
 modal();
 calc();
 tabs();
+slider();
