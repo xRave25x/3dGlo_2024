6e2aac2c6a01ed1a38513d031e7167a368f0bde3
xRave25x <Andyuson24@gmail.com>
2024-01-26 02:08:20 +0200

    Update modal.js

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,39 +1,30 @@
 const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const popup = document.querySelector('.popup');
-    const popupClose = popup.querySelector('.popup-close');
-    const popupContent = popup.querySelector('.popup-content');
-    const screenWidth = window.screen.width;
-    console.log(screenWidth);
+    const screenWidth = window.innerWidth;
 
     const animationPopup = () => {
         if (screenWidth > 768) {
-            const start = Date.now();
-            const draw = timePassed => {
-                popupContent.style.left = timePassed / 5 + 'px';
-            };
-            const timer = setInterval(() => {
-                const timePassed = Date.now() - start;
-                if (timePassed >= 4000) {
-                    clearInterval(timer);
+            let op = 0;
+            setTimeout(function func() {
+                if (op > 1)
                     return;
-                }
-                draw(timePassed);
-            }, 20);
-            popup.style.display = 'block';
-        } else {
-            popup.style.display = 'block';
+                popup.style.display = 'block';
+                popup.style.opacity = op;
+                op += 0.1;
+                setTimeout(func, 60);
+            }, 60);
         }
     };
 
     buttons.forEach(button => button.addEventListener('click', animationPopup));
 
 
-
-    popupClose.addEventListener('click', () => {
-        popup.style.display = 'none';
+    popup.addEventListener('click', e => {
+        if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+            popup.style.display = 'none';
+        }
     });
-
 
 };
 
